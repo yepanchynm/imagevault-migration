c41d274bc632377b4bf2deaa474fbbb959792ee4
m.yepanchyn <yepanchyn.m@brights.io>
2024-12-19 13:31:47 +0200

    WIP: replace images links

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ const getStoryFilename = (name) => {
 
 async function bootstrap() {
     try {
-        const data = await storyblokService.getStoryBySlug('en/main/demos/brights/demo-product-maksym')
+        const data = await storyblokService.getStoryBySlug('en/main/demos/brights/demo-product')
 
         const replaceStoryImageService = new ReplaceStoryImagesService(data)
         await promises.writeFile(getStoryFilename('demo-product'), JSON.stringify(data, null, 2));
@@ -27,13 +27,18 @@ async function bootstrap() {
         const newData = replaceStoryImageService.replace().get()
         await promises.writeFile(getStoryFilename('demo-product-replaced'), JSON.stringify(newData, null, 2));
 
-        if (newData.id) {
-            const response = await storyblokService.updateStory(newData.id, newData, {
-                force_update: 1,
-                publish: 1,
-            })
-            console.log(`${newData.id} updated`)
-        }
+        // if (newData.id) {
+        //     const response = await storyblokService.updateStory(newData.id, newData, {
+        //         force_update: 1,
+        //         publish: 1,
+        //     })
+        //
+        //     if (response.status === 200) {
+        //         console.log(`${newData.id} updated`)
+        //     } else {
+        //         console.log(`Failed to update story ID ${newData.id}`)
+        //     }
+        // }
     } catch (err) {
         console.error(err);
     }
--- src/replaceStoryImagesService.js
+++ src/replaceStoryImagesService.js
@@ -1,5 +1,25 @@
 import {bypassObjectEntries} from "./helpers/bypassObjectEntries.js";
 
+function removeObjectByValue(obj, key, value) {
+    if (Array.isArray(obj)) {
+        return obj.filter(item => {
+            if (typeof item === "object") {
+                return removeObjectByValue(item, key, value);
+            }
+            return item[key] !== value;
+        });
+    } else if (typeof obj === "object" && obj !== null) {
+        for (const [k, v] of Object.entries(obj)) {
+            if (k === key && v === value) {
+                delete obj[k];
+            } else if (typeof v === "object") {
+                obj[k] = removeObjectByValue(v, key, value);
+            }
+        }
+    }
+    return obj;
+}
+
 export class ReplaceStoryImagesService {
     #storyData
     result = null
@@ -15,12 +35,18 @@ export class ReplaceStoryImagesService {
         bypassObjectEntries(this.#storyData,
             'plugin',
             'image-vault',
-            async (item) => {
-                item.item?.MediaConversions?.forEach(media => {
-                    const newSrc = `https://a.storyblok.com/f/149538/2000x1125/53779230e2/tobii-eye-glasses.jpg`
-                    media.Url = newSrc
-                    media.Html = media.Html.replace(/src="[^"]*"/, `src="${newSrc}"`)
-                })
+            async (item, parent) => {
+                const [mediaConversation] = item.item?.MediaConversions;
+                const {_uid} = item
+
+                if (!mediaConversation) {
+                    console.error('No image')
+                    return;
+                }
+
+                removeObjectByValue(parent, '_uid', _uid)
+
+                console.log(_uid, parent._uid)
             }
         )
 
