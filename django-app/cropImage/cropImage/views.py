3760245335f0e564891b52e626aae35954857a4f
Vladyslav Diachenko <vladyslav.diachenko@brights.io>
2025-01-31 10:08:02 +0200

    Update comments

--- django-app/cropImage/cropImage/views.py
+++ django-app/cropImage/cropImage/views.py
@@ -11,10 +11,10 @@ from PIL import Image
 
 
 def get_image_from_url(url):
-    """Отримання зображення з URL у вигляді NumPy масиву"""
+    """Retrieve an image from a URL as a NumPy array"""
     response = requests.get(url)
     if response.status_code != 200:
-        raise ValueError("Не вдалося завантажити зображення")
+        raise ValueError("Failed to download image")
 
     content_type = response.headers.get("Content-Type", "")
 
@@ -24,7 +24,7 @@ def get_image_from_url(url):
         img_array = np.array(bytearray(response.content), dtype=np.uint8)
         img = cv2.imdecode(img_array, cv2.IMREAD_UNCHANGED)
         
-        # Якщо формат не підтримується OpenCV, використовуємо PIL
+        # If format is unsupported by OpenCV, use PIL
         if img is None:
             img = convert_with_pil(response.content)
 
@@ -32,7 +32,7 @@ def get_image_from_url(url):
 
 
 def convert_svg_to_png(svg_data):
-    """Конвертація SVG у PNG"""
+    """Convert SVG to PNG"""
     png_data = BytesIO()
     cairosvg.svg2png(bytestring=svg_data, write_to=png_data)
     img_array = np.array(bytearray(png_data.getvalue()), dtype=np.uint8)
@@ -40,13 +40,13 @@ def convert_svg_to_png(svg_data):
 
 
 def convert_with_pil(image_data):
-    """Перетворення нестандартного формату (TIF, WEBP) у OpenCV"""
+    """Convert non-standard formats (TIF, WEBP) to OpenCV"""
     image = Image.open(BytesIO(image_data))
     return cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR)
 
 
 def find_crop_coordinates(original_url, cropped_url):
-    """Пошук координат обрізаного зображення"""
+    """Find coordinates of a cropped image"""
     original = get_image_from_url(original_url)
     cropped = get_image_from_url(cropped_url)
 
@@ -69,10 +69,10 @@ def check_crop(request):
             cropped_url = data.get("cropped_url")
 
             if not original_url or not cropped_url:
-                return JsonResponse({"error": "Не вказано посилання"}, status=400)
+                return JsonResponse({"error": "Missing URLs"}, status=400)
 
             if not is_valid_image_url(original_url) or not is_valid_image_url(cropped_url):
-                return JsonResponse({"error": "Неправильний формат файлу"}, status=400)
+                return JsonResponse({"error": "Invalid file format"}, status=400)
 
             x1, y1, x2, y2 = find_crop_coordinates(original_url, cropped_url)
 
@@ -83,7 +83,7 @@ def check_crop(request):
 
 
 def is_valid_image_url(url):
-    """Перевіряє, чи є URL-адреса зображенням"""
+    """Check if a URL is an image"""
     image_extensions = (".jpg", ".jpeg", ".png", ".gif", ".svg", ".tif", ".tiff", ".webp")
     mimetype, _ = mimetypes.guess_type(url)
     return mimetype and mimetype.startswith("image/") or url.lower().endswith(image_extensions)
