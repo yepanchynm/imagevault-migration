5e481eba159582448087e1a9685e58b9c9f69c2f
Vladyslav Diachenko <vladyslav.diachenko@brights.io>
2025-03-05 12:56:32 +0200

    Delete unneeded code for script

--- django-app/cropImage/cropImage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('check-crop/', views.check_crop),
-    path('generate-excel/', views.generate_excel)
-]
